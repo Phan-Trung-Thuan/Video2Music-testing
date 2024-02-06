2dec6bc8bd0e7f43017e0b8ab95c5be5545b5b4a
Phan Trung Thuận <thuantk000@gmail.com>
2024-02-06 21:35:39 +0700

    Init first code of motion and semantic feature

--- dataset/feature_extraction.py
+++ dataset/feature_extraction.py
@@ -1,32 +1,52 @@
 from moviepy.editor import VideoFileClip
 import os
+import numpy as np
 from function import *
 
 idList = get_id_list(idlist_path='./dataset/vevo_meta/idlist.txt')
 
-download_video_path = './dataset/video'
-# If the directory is not exist then create it
-if not os.path.exists(download_video_path):
-    os.makedirs(download_video_path)
+paths = ['./dataset/video', 
+         './dataset/vevo_note_density', 
+         './dataset/vevo_loudness', 
+         './dataset/vevo_chord_sequence', 
+         './dataset/vevo_key', 
+         './dataset/vevo_emotion', 
+         './dataset/vevo_scene_offset', 
+         './dataset/vevo_motion', 
+         './dataset/vevo_semantic']
 
-# Extract feature for each video - audio pair
+for path in paths:
+    # If the directory is not exist then create it
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+# Extract feature for each VIDEO - AUDIO pair
 for index, id in idList:
-    video_path = os.path.join(download_video_path, f'{index}.mp4')
+    video_path = os.path.join(paths[0], f'{index}.mp4')
 
     # If the video is not downloaded then download it
     if not os.path.exists(video_path):
-        download_youtube_video(id, download_video_path, f'{index}.mp4')
+        download_youtube_video(id, paths[0], f'{index}.mp4')
 
     video = VideoFileClip(video_path)
     video_no_sound = video.without_audio()
     audio = video.audio()
 
-    # MUSIC FEATURE === Note density ===
-    # MUSIC FEATURE === Loudness ===
-    # MUSIC FEATURE === Chord sequence ===
-    # MUSIC FEATURE === Key ===
+    # MUSIC FEATURE === Note density === 1
+    # MUSIC FEATURE === Loudness === 2
+    # MUSIC FEATURE === Chord sequence === 3
+    # MUSIC FEATURE === Key === 4
 
-    # VIDEO FEATURE === Emotion ===
-    # VIDEO FEATURE === Scene offset ===
-    # VIDEO FEATURE === Motion ===
-    # VIDEO FEATURE === Semantic ===
+    # VIDEO FEATURE === Emotion === 5
+    # VIDEO FEATURE === Scene offset === 6
+    # VIDEO FEATURE === Motion === 7
+    motion_feature_path = os.path.join(paths[7], f'{index}_motion.npy')
+    # If this video does not extract motion feature then extract it
+    if not os.path.exists(motion_feature_path):
+        pass
+
+    # VIDEO FEATURE === Semantic === 8
+    semantic_feature_path = os.path.join(paths[8], f'{index}_semantic.npy')
+    # If this video does not extract semantic feature then extract it
+    if not os.path.exists(semantic_feature_path):
+        pass
