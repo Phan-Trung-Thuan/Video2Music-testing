014603d467cb24021cee7947cd30f2f8e5a74589
taib2111862 <155361204+taib2111862@users.noreply.github.com>
2024-02-21 23:45:55 +0700

    Hi Thuan. Please check out my scene offset extract results. I think that there'll be a small problem if the video changes scenes too frequently (<1s). Hope that you can figure out the solution soon. Many thanks!

---
+++ scene_offset.py
@@ -0,0 +1,45 @@
+from scenedetect import SceneManager, open_video, FrameTimecode, AdaptiveDetector
+
+
+def find_scenes(video_path):
+    video = open_video(video_path)
+    scene_manager = SceneManager()
+    scene_manager.add_detector(
+        AdaptiveDetector())
+    # Detect all scenes in video from current position to end.
+    scene_manager.detect_scenes(video, show_progress=False)
+    # `get_scene_list` returns a list of start/end timecode pairs
+    # for each scene that was found.
+    return scene_manager.get_scene_list()
+
+
+def scene_offset(video_path):
+    scene_offset_list = []
+    scene_list = find_scenes(video_path)
+    for i, scene in enumerate(scene_list):
+        scene_start_frame = scene[0].get_frames()
+        scene_end_frame = scene[1].get_frames()
+        scene_start_time = int(FrameTimecode(timecode=scene_start_frame, fps=25.00).get_seconds())
+        scene_end_time = int(FrameTimecode(timecode=scene_end_frame, fps=25.00).get_seconds())
+        if scene_start_time == scene_end_time:
+            scene_offset_list.append((scene_start_time, 0))
+        else:
+            sec = scene_start_time
+            scene_sec = 0
+            for j in range(scene_start_time, scene_end_time):
+                if sec == scene_end_time:
+                    break
+                scene_offset_list.append((sec, scene_sec))
+                sec += 1
+                scene_sec += 1
+    return scene_offset_list
+
+
+def main():
+    video = scene_offset('dataset/video/001.mp4')
+    for i in range(len(video)):
+        print('%d, %d' % (video[i][0], video[i][1]))
+
+
+if __name__ == '__main__':
+    main()
